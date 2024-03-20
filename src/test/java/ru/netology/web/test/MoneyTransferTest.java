8eaa75222888f80b75d4a7c07ea0fd2ed8c3e075
Denwin <biscooter@mail.ru>
2024-03-20 15:50:04 +0600

    UpToDate

--- src/test/java/ru/netology/web/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -1,11 +1,16 @@
 package ru.netology.web.test;
 
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.chrome.ChromeOptions;
 import ru.netology.web.data.DataHelper;
 import ru.netology.web.page.DashBoardPage;
 import ru.netology.web.page.LoginPage;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static ru.netology.web.data.DataHelper.*;
@@ -16,6 +21,13 @@ public class MoneyTransferTest {
 
     @BeforeEach
     void setup() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        Map<String, Object> prefs = new HashMap<String, Object>();
+        prefs.put("credentials_enable_service",false);
+        prefs.put("password_manager_enabled",false);
+        options.setExperimentalOption("prefs",prefs);
+        Configuration.browserCapabilities =options;
         var loginPage = open("http://localhost:9999", LoginPage.class);
         var authInfo = getAuthInfo();
         var verificationPage = loginPage.validLogin(authInfo);
